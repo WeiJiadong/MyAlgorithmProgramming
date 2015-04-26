2a91b7526a37607a777377b125bd48fbf417ff23
WeiJiadong <3534704569@qq.com>
2015-04-26 12:43:23 +0800

    5
    5

--- 全排列(无重复值).cpp
+++ 全排列(无重复值).cpp
@@ -13,36 +13,36 @@ void fullPermutation(T &a, unsigned int k, unsigned int n);
  
 int main(){
 	//测试数据以整型数据为例 
-	vector<int> array;
-	unsigned int n;
+    vector<int> array;
+    unsigned int n;
 	
-	cin >> n;
+    cin >> n;
 	
-	for (int i = 0; i < n; i++){
-		int temp;
-		cin >> temp;
-		array.push_back(temp);
-	} 
-	cout << endl << "全排列结果为:" << endl;
+    for (int i = 0; i < n; i++){
+	    int temp;
+	    cin >> temp;
+	    array.push_back(temp);
+    } 
+    cout << endl << "全排列结果为:" << endl;
 	
-	fullPermutation(array, 0, n);	
+    fullPermutation(array, 0, n);	
 	
-	return 0;
+    return 0;
 }
 template <typename T>
 void fullPermutation(T &a, unsigned int k, unsigned int n){
     if (k == n){	//如果k和m相等 
-		for (int i = 0; i < n; i++){
-			cout << a[i] << ' ';
-		} 
-		cout << endl;
-	}
-	for (int i = k; i < n; i++){
-		//固定k，分别与后面交换 
-		swap(a[k], a[i]);
-		//再固定k+1与后面继续进行全排列 
-		fullPermutation(a, k + 1, n);
-		//调用结束再换回来 
-		swap(a[k], a[i]);
-	} 
+        for (int i = 0; i < n; i++){
+            cout << a[i] << ' ';
+       } 
+        cout << endl;
+    }
+    for (int i = k; i < n; i++){
+        //固定k，分别与后面交换 
+        swap(a[k], a[i]);
+        //再固定k+1与后面继续进行全排列 
+        fullPermutation(a, k + 1, n);
+        //调用结束再换回来 
+        swap(a[k], a[i]);
+    } 
 }
